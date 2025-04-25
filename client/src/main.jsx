985d7a1446a21d481a98423ca3a555a219d875fc
AlanJames2005 <alanvj2005@gmail.com>
2025-04-25 09:59:38 +0530

    Updated project with latest changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <ThemeProvider>
       <AuthProvider>
         <WorkProvider>
-          <App />
+    <App />
         </WorkProvider>
       </AuthProvider>
     </ThemeProvider>
